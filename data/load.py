97620c0e3f735e3962dd8bd38d1e7fccbe5cb347
riccardovittimberga <162723110+riccardovittimberga@users.noreply.github.com>
2025-03-18 16:55:36 +0100

    Update load.py

--- data/load.py
+++ data/load.py
@@ -1,6 +1,7 @@
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as T
 import os
+import shutil
 
 with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
     for line in f:
